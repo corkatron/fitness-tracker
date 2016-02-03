f701dca36ae99259b2aa8ff9c22d56a9d5fa1052
corkatron <corky1220@gmail.com>
2016-02-03 15:04:38 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FitnessTracker::Application.config.secret_key_base = '72eb77cf0fc14fa49eb644ed96ab87069c544c7a7eb2a462df8ebb4e9dd613fdcd45e8908a047e7405d54dd9a6ee7db84dd28e61ef6e223e5975196def30875f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FitnessTracker::Application.config.session_store :cookie_store, key: '_fitness_tracker_session'
